7b28f692555f828742f4470ec1fbd9fa43bfbcff
Lucas <lucasalexandre_82@hotmail.com>
2022-09-05 12:09:52 -0300

    Criando funções

--- intermediario/banco.php
+++ intermediario/banco.php
@@ -1,5 +1,16 @@
 <?php
 
+function sacar($conta, $valorASacar)
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não pode sacar um valor acima do seu saldo");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
 function exibeMensagem($mensagem)
 {
     echo $mensagem . PHP_EOL;
@@ -11,17 +22,8 @@ $contasCorrentes = [
     '123.456.789-30' => ['titular' => 'Maria', 'saldo' => 2000]
 ];
 
-if (500 > $contasCorrentes['123.456.789-30']['saldo']) {
-    exibeMensagem("Você não pode sacar um valor acima do seu saldo");
-} else {
-    $contasCorrentes['123.456.789-30']['saldo'] -= 500;
-}
-
-if (500 > $contasCorrentes['123.456.789-20']['saldo']) {
-    exibeMensagem("Você não pode sacar um valor acima do seu saldo");
-} else {
-    $contasCorrentes['123.456.789-20']['saldo'] -= 500;
-}
+$contasCorrentes['123.456.789-30'] = sacar($contasCorrentes['123.456.789-30'], 500);
+$contasCorrentes['123.456.789-20'] = sacar($contasCorrentes['123.456.789-20'], 500);
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
