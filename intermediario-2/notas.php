6607e9a9faea0f90f6836eefb80c1755b5c29353
Lucas <lucasalexandre_82@hotmail.com>
2022-09-18 13:28:15 -0300

    Verificando se uma chave é diferente de nula, verificando se um valor existe, bucando a chave do valor caso ele exista, e aprendendo a fazer isso de forma mais ou menos estrita no array

--- intermediario-2/notas.php
+++ intermediario-2/notas.php
@@ -1,7 +1,7 @@
 <?php
 
 $notas = [
-    'Vinicius' => 6,
+    'Vinicius' => null,
     'João' => 8,
     'Ana' => 10,
     'Roberto' => 7,
@@ -17,5 +17,17 @@ if (is_array($notas)) {
 
 var_dump(array_is_list($notas));
 
+// array_key_exists = Verifica se uma chave (índice) existe no array
 echo 'Vinicius fez a prova?' . PHP_EOL;
 var_dump(array_key_exists('Vinicius', $notas));
+
+// isset = Verifica se uma chave (índice) existe no array e se o valor dela não é nulo
+echo 'Vinicius fez a prova?' . PHP_EOL;
+var_dump(isset($notas['Vinicius']));
+
+// isset = Verifica se um valor existe no array
+echo 'Alguém tirou nota 10?' . PHP_EOL;
+var_dump(in_array(10, $notas, strict: true)); // strict = faz uma comparação mais rigorosa
+
+echo 'Quem tirou 10?' . PHP_EOL;
+echo array_search(10, $notas, strict: true);
